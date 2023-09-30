001df13162a2d3ea8b7f36591ae513ded6c57010
farman ali ujjan <farman20ali@gmail.com>
2023-09-30 19:22:19 +0500

    disk usage correction

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # git_code
 for learning git commands and version controlling
+changing the file --->>README.md
---
+++ disk_usage_fixed.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import shutil
+import sys
+
+def checkdiskusage(disk,min_absolute, min_percent):
+    """Returns true if there is enough free disk space, false otherwise"""
+    du=shutil.disk_usage(disk)
+    #calcuate the percentage of free space
+    percent_free=100*du.free/du.total
+    gigabytes_free =du.free/2**30
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return False
+    return True
+
+#check for at least 2gb and 18%free
+if not checkdiskusage('/',2,18):
+    print("Error: Not enough disk space")
+    sys.exit(1)
+
+print("everything ok")
+sys.exit(0)
