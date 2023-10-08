85f132b1cc81f3cfd9986d4248e63e8e90569745
farman ali ujjan <farman20ali@gmail.com>
2023-10-08 16:56:22 +0500

    refactored the code

--- disk_usage.py
+++ disk_usage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import shutil
 import sys
+import os
 
 def checkdiskusage(disk,min_gb, min_percent):
     """Returns true if there is enough free disk space, false otherwise"""
@@ -9,17 +10,34 @@ def checkdiskusage(disk,min_gb, min_percent):
     percent_free=100*du.free/du.total
     gigabytes_free =du.free/2**30
     if percent_free < min_percent or gigabytes_free < min_gb:
-        return False
-    return True
+        return True
+    return False
 
+def reboot_check():
+    if os.path.exists('/var/run/reboot-required'):
+        return True
+    else:
+        return False
 def check_root_full():
    """Returns True if the root partiition is full, False otherwise."""
    return checkdiskusage('/',2,20);
 #check for at least 2gb and 18%free
 def main():
-    if not check_root_full():
-        print("Error: Disk Full. Not enough disk space")
+    checks=[(reboot_check,"reboot is required"),(check_root_full,"Disk is Full")]
+    for method,msg in checks:
+        error=False
+        if method():
+            print(msg)
+            error=True
+    if error:
         sys.exit(1)
+
+    # if not check_root_full():
+    #     print("Error: Disk Full. Not enough disk space")
+    #     sys.exit(1)
+    # if reboot_check():
+    #     print("reboot is required")
+    #     sys.exit(1)
     print("everything ok")
     sys.exit(0)
 
